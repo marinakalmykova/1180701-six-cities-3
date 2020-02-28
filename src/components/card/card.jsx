67e6f6c54cbab862ec638622ce20cfe9068a35a0
marinakalmykova <marina@meyvndigital.co.uk>
2020-02-28 14:32:33 +0700

    правка по ревью

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -21,7 +21,7 @@ const Card = ({offer, onHeaderClick, onCardHover}) => {
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={`place-card__bookmark-button ` + (isBookmark && `place-card__bookmark-button--active `) + `button`} type="button">
+          <button className={`place-card__bookmark-button ${isBookmark && `place-card__bookmark-button--active`} button`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
